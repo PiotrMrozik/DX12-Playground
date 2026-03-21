913b6652ebc5d8776f237fbeecf301cd8bfd7178
Piotr Mrozik <piter012@vp.pl>
2026-03-21 20:28:30 +0100

    Write super-simple phong for cubes

--- DX12-Playground/Sample/Shaders/PixelShader.hlsl
+++ DX12-Playground/Sample/Shaders/PixelShader.hlsl
@@ -1,9 +1,18 @@
 struct PixelShaderInput
 {
     float4 Color : COLOR;
+    float4 Position : SV_Position;
+    float4 PositionWorld : POSITIONWORLD;
+    float4 Normal : NORMAL;
 };
 
 float4 main(PixelShaderInput IN) : SV_Target
 {
-    return IN.Color;
+   
+    float3 norm     = normalize(IN.Normal.xyz);
+    float3 lightDir = normalize(float3(12.0f, 5.0f, 0.0f) - IN.PositionWorld.xyz);
+    float  diff     = max(dot(norm, lightDir), 0.0f);
+    float3 diffuse  = diff * float3(1.5f, 1.5f, 1.5f);
+
+    return float4(diffuse, 1.0f);
 }
--- DX12-Playground/Sample/Shaders/VertexShader.hlsl
+++ DX12-Playground/Sample/Shaders/VertexShader.hlsl
@@ -1,24 +1,35 @@
 struct VertexPosColor
 {
     float3 Position : POSITION;
-    float3 Color    : COLOR;
+    float3 Color : COLOR;
+    float3 Normal : NORMAL;
 };
 
 struct VertexShaderOutput
 {
-    float4 Color    : COLOR;
+    float4 Color : COLOR;
     float4 Position : SV_Position;
+    float4 PositionWorld : POSITIONWORLD;
+    float4 Normal : NORMAL;
 };
 
-cbuffer ModelViewProjection : register(b0)
+cbuffer WorldMatrix : register(b0)
 {
-    matrix MVP;
+    matrix World;
+}
+
+cbuffer ViewProjection : register(b1)
+{
+    matrix VP;
 }
 
 VertexShaderOutput main(VertexPosColor IN)
 {
     VertexShaderOutput OUT;
-    OUT.Position = mul(MVP, float4(IN.Position, 1.0f));
-    OUT.Color = float4(IN.Color, 1.0f);
+    float4 worldPos  = mul(World, float4(IN.Position/10, 1.0f));
+    OUT.Position      = mul(VP, worldPos);
+    OUT.PositionWorld = worldPos;
+    OUT.Color         = float4(IN.Color, 1.0f);
+    OUT.Normal        = float4(mul((float3x3)World, IN.Normal), 0.0f);
     return OUT;
 }
